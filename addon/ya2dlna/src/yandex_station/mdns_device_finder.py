b90360131df678ba91daa31957afc533e9e36aaa
key4zed <key-zed@mail.ru>
2026-04-05 08:54:33 +0300

    fix

--- addon/ya2dlna/src/yandex_station/mdns_device_finder.py
+++ addon/ya2dlna/src/yandex_station/mdns_device_finder.py
@@ -2,6 +2,7 @@ from core.logging.setup import setup_logging  # noqa
 import ipaddress
 from logging import getLogger
 from time import sleep
+from typing import List, Dict
 
 from zeroconf import (ServiceBrowser, ServiceListener, ServiceStateChange,
                       Zeroconf)
@@ -13,11 +14,12 @@ class DeviceFinder(ServiceListener):
     """Класс для поиска устройств Yandex Station в сети."""
 
     def __init__(self):
-        self.device = {}
+        self.devices: List[Dict] = []
         self.zeroconf = Zeroconf()
 
     def find_devices(self, type_="_yandexio._tcp.local."):
         """Поиск устройств Yandex Station в сети."""
+        self.devices.clear()
         self.browser = ServiceBrowser(
             zc=self.zeroconf,
             type_=type_,
@@ -40,13 +42,18 @@ class DeviceFinder(ServiceListener):
             }
             logger.info(f"Properties: {properties}")
 
-            self.device = {
+            device = {
                 "device_id": properties["deviceId"],
                 "platform": properties["platform"],
                 "host": str(ipaddress.ip_address(info.addresses[0])),
                 "port": info.port,
             }
-            logger.info(f"Найдены устройства: {self.device}")
+            # Проверяем, нет ли уже такого device_id в списке
+            if not any(d["device_id"] == device["device_id"] for d in self.devices):
+                self.devices.append(device)
+                logger.info(f"Добавлена Яндекс Станция: {device}")
+            else:
+                logger.debug(f"Яндекс Станция {device['device_id']} уже в списке")
 
         except Exception as e:
             logger.error(f"Error: {e}")
